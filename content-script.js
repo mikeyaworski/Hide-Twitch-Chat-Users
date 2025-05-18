c25e1ed510b775aadf8019d1473d9c9cc04f4fe5
Michael Yaworski <mikeyaworski@hotmail.com>
2025-05-17 20:58:22 -0400

    Feat: Prefer CSS to hide elements
    Adds option to hide all mentions
    Includes mentions in message body
    and reply lines
    
    Closes #3

--- content-script.js
+++ content-script.js
@@ -1,58 +1,103 @@
+let intervalId = null;
+
 // defaults while we fetch from storage
 let hide = false;
+let hideMentions = false;
 let usernames = [];
 
 function parseUsernamesStr(usernamesStr = '') {
-  return usernamesStr.split(/[,\s+]/g).map(u => u.toLowerCase());
+  return usernamesStr.split(/[,\s+]/g).map(u => u.toLowerCase().trim()).filter(Boolean);
 }
 
-chrome.storage.sync.get(['hide', 'usernames'], res => {
-  hide = res.hide != null ? res.hide : true; // default to true if yet to be defined
+function upsertStyleSheet(css) {
+  let style = document.getElementById('hide-twitch-chat-users');
+  if (!style) {
+    style = document.createElement('style');
+    style.id = 'hide-twitch-chat-users';
+    document.head.appendChild(style);
+  }
+  style.textContent = css;
+}
+
+function generateStyleSheet() {
+  if (!hide) return '';
+  return usernames.map(username => {
+    const styles = [
+      `.chat-line__message[data-a-user="${username}"] {
+        display: none !important;
+      }`,
+      // Viewer List
+      `#community-tab-content div:has(> .tw-link[aria-label~="${username}" i]),
+      [role="listitem"]:has(> .chat-viewers-list__button[data-username="${username}" i]) {
+          display: none !important;
+        }`,
+    ];
+    if (hideMentions) {
+      // The DOM differs depending on if the reply/mention is of an arbitrary user vs the broadcaster
+      styles.push(`
+        .chat-line__message-container > div:has( p[title~="@${username}" i]) {
+          display: none !important;
+        }
+        .chat-line__message[aria-label~="@${username}" i],
+        .chat-line__message[aria-label~="${username}" i],
+        .chat-line__message[aria-label~="${username}," i] {
+          display: none !important;
+        }
+      `);
+    }
+    return styles.join('\n');
+  }).join('\n');
+}
+
+function getTextContainsBlockedUserMention(text) {
+  return usernames.some(username => new RegExp(`@${username}($|([^a-zA-Z0-9_]))`, 'i').test(text?.toLowerCase()))
+}
+
+// The DOM for 7TV is not friendly to CSS selectors, so we need to use JS
+function pollDom() {
+  if (intervalId != null) clearInterval(intervalId);
+  intervalId = setInterval(() => {
+    if (!hide) return;
+    const msgs = document.querySelectorAll('.seventv-message');
+    msgs.forEach(msg => {
+      const authorEl = msg.querySelector('.seventv-chat-user-username');
+      if (usernames.includes(authorEl?.innerText.toLowerCase())) {
+        msg.style.display = 'none';
+      }
+      if (hideMentions) {
+        const messageEl = msg.querySelector('.seventv-chat-message-body');
+        if (messageEl) {
+          const containsBlockedUser = getTextContainsBlockedUserMention(messageEl.innerText);
+          if (containsBlockedUser) {
+            msg.style.display = 'none';
+          }
+        }
+        const replyToMessageEl = msg.querySelector('.seventv-reply-message-part');
+        if (replyToMessageEl) {
+          const containsBlockedUser = getTextContainsBlockedUserMention(replyToMessageEl.innerText);
+          if (containsBlockedUser) {
+            const replyPart = msg.querySelector('.seventv-reply-part');
+            if (replyPart) replyPart.style.display = 'none';
+          }
+        }
+      }
+    });
+  }, 100);
+}
+
+chrome.storage.sync.get(['hide', 'hideMentions', 'usernames'], res => {
+  hide = res.hide != null ? res.hide : true;
+  hideMentions = res.hideMentions != null ? res.hideMentions : false;
   usernames = parseUsernamesStr(res.usernames);
+  upsertStyleSheet(generateStyleSheet());
+  pollDom();
 });
 
 chrome.storage.onChanged.addListener(res => {
   if (chrome.runtime.lastError) return;
   if (res.hide) hide = res.hide.newValue;
+  if (res.hideMentions) hideMentions = res.hideMentions.newValue;
   if (res.usernames) usernames = parseUsernamesStr(res.usernames.newValue);
+  upsertStyleSheet(generateStyleSheet());
+  pollDom();
 });
-
-function filterViewerList() {
-  const viewerListButtons = document.querySelectorAll('#community-tab-content .tw-link');
-  viewerListButtons.forEach(btn => {
-    if (usernames.includes(btn.innerText.toLowerCase())) {
-      btn.parentElement.style.display = 'none';
-    }
-  });
-}
-
-// Alternatively, we can hide the messages by default and only show them if the username
-// is found to not be one of the hidden ones
-setInterval(() => {
-  if (!hide) return;
-  const msgs = document.querySelectorAll('.chat-line__message, .seventv-message');
-  msgs.forEach(msg => {
-    const authorEl = msg.querySelector('.chat-author__display-name, .seventv-chat-user-username');
-    if (usernames.includes(authorEl?.innerText.toLowerCase())) {
-      msg.style.display = 'none';
-    }
-    // Vanilla Twitch
-    const replyTo = msg.getAttribute('aria-label')?.match(/Replying to ([^,]+)/)?.[1];
-    if (replyTo && usernames.includes(replyTo.toLowerCase())) {
-      const replyToEl = msg.querySelector('p[title^=Replying]') || msg.querySelector('p[class^=CoreText]');
-      if (replyToEl) {
-        replyToEl.style.display = 'none';
-      }
-    }
-    // 7TV
-    const replyToMessageEl = msg.querySelector('.seventv-reply-message-part');
-    if (replyToMessageEl) {
-      const replyTo = replyToMessageEl.innerText?.toLowerCase().match(/replying to @([^:]+)/)?.[1];
-      if (usernames.includes(replyTo.toLowerCase())) {
-        const replyPart = msg.querySelector('.seventv-reply-part');
-        if (replyPart) replyPart.style.display = 'none';
-      }
-    }
-  });
-  filterViewerList();
-}, 100);
--- popup.js
+++ popup.js
@@ -1,14 +1,18 @@
-chrome.storage.sync.get(['hide', 'usernames'], ({ hide, usernames }) => {
-  if (hide == null) hide = true; // default to true if yet to be defined
+chrome.storage.sync.get(['usernames', 'hide', 'hideMentions'], ({ hide, usernames, hideMentions }) => {
+  if (hide == null) hide = true;
+  if (hideMentions == null) hideMentions = false;
   document.getElementById('hide').checked = hide;
+  document.getElementById('hide-mentions').checked = hideMentions;
   if (usernames != null) document.getElementById('usernames').value = usernames;
 });
 
 function saveChanges() {
   const usernames = document.getElementById('usernames').value;
   const hide = document.getElementById('hide').checked;
-  chrome.storage.sync.set({ usernames, hide });
+  const hideMentions = document.getElementById('hide-mentions').checked;
+  chrome.storage.sync.set({ usernames, hide, hideMentions });
 }
 
 document.getElementById('usernames').addEventListener('input', saveChanges);
 document.getElementById('hide').addEventListener('click', saveChanges);
+document.getElementById('hide-mentions').addEventListener('click', saveChanges);
